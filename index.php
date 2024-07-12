33543d95a4b7cfb86eeaa3fbd85344cb7e811d1e
mohittiwariuk02 <96790611+mohittiwariuk02@users.noreply.github.com>
2024-07-12 11:30:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>My PHP App</title>
-</head>
-<body>
-    <h1>Welcome to my PHP App!</h1>
-    <p>This is a simple PHP application hosted on AWS Elastic Beanstalk using Docker.</p>
-    <?php
-        echo "<p>Today's date is " . date("Y-m-d H:i:s") . "</p>";
-    ?>
-</body>
-</html>
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+phpinfo(INFO_MODULES);
+
+?>
